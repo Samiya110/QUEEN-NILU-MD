6e729c2fb118799a35c83038396b773613d0ffde
Samiya110 <86985448+Samiya110@users.noreply.github.com>
2023-10-03 22:03:26 +0530

    Update config.js

--- config.js
+++ config.js
@@ -14,24 +14,24 @@ function convertToBool(text, fault = 'true') {
 
 // PUT YOUR SESSION ID HERE  ##Required 
 
-global.SESSION_ID = '' // PUT your session ID BY scan QR 
+global.SESSION_ID = 'QueenNilu;;;suA3WSIK#VAyKzrdaAwZNlZ742Im-eW7WcgSs_TKSPtJ-YJAwiM8' // PUT your session ID BY scan QR 
 
 // databse URL  ##Required 
-global.POSTGRESQL_URL = ''
+global.POSTGRESQL_URL = 'postgres://tin_tin_user:ew7zUUD7MWV4zKua6M4b6oA3R8e9xGwD@dpg-cke3ttkgonuc73a9bsc0-a.frankfurt-postgres.render.com/tin_tin'
 
 
 
 // ⚙️ OWNER SETTINGS  ⚙️
 
-global.OWNER_NUMBER = '94767438882'
+global.OWNER_NUMBER = '94704342100'
 
-global.OWNER_NAME = 'Janiya_X'
+global.OWNER_NAME = 'Sameera C Athapaththu'
 
 global.TIMEZONE = 'Asia/Colombo' 
 
-global.INBOX_BLOCK_MESSAGE = '' // Inboc Block Message
+global.INBOX_BLOCK_MESSAGE = 'Sorry.' // Inboc Block Message
 
-global.INBOX_BLOCK = 'true' // TO Inbox Block On put true
+global.INBOX_BLOCK = 'false' // TO Inbox Block On put true
 
 global.READ_MESSAGE = "true" 
 
@@ -41,19 +41,19 @@ global.OPENAI_API_KEY = "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"
 ⚙️ BOT  SETTINGS  ⚙️
 */
 
-global.BOT_NAME = 'ǫᴜᴇᴇɴ ɴɪʟᴜ'
+global.BOT_NAME = 'Queen Schii'
  
 global.BOT_OFFLINE = false    //  Always Onlne -  | false offline -True
 
 global.STICKER_NAME = '@Janiya 🤹‍♂️' //sticker
 
-global.FOOTER = 'Queen Nilu 2023 '
+global.FOOTER = 'Queen Schii 2023 '
 
 global.LANG = 'EN' // Language (EN-  TO english  ,  SI- TO sinhala )
 
 global.PREFIX = '.' // Bot Prefix
 
-global.CAPTION = 'ǫᴜᴇᴇɴ ɴɪʟᴜ  © 2023' // Caption
+global.CAPTION = 'Queen Schii  © 2023' // Caption
 
 global.ALIVELOGO = 'https://telegra.ph/file/1e63f0ee90304a12767c7.jpg' // Set Alive Logo link 
 
@@ -65,7 +65,7 @@ global.ANTI_BAD = 'true' // Antibad (on = True , Off = False )
 
 global.ANTI_BAD_ACTION = false
 
-global.ANTI_LINK = 'false' //Anti Link (on = True , Off = False )
+global.ANTI_LINK = 'true' //Anti Link (on = True , Off = False )
 
 global.ANTI_BOTS = true
 
@@ -82,14 +82,14 @@ global.SEX_DL = 'false' // 18 + video dl
 
 global.AUTO_STICKER = true
 
-global.AUTO_REPLY = true
+global.AUTO_REPLY = false
 
-global.VOICE_REPLY = true
+global.VOICE_REPLY = false
 
 
 /* V card */
 
-global.ytname = "YT: janithsadanuwan" //ur yt chanel name
-global.socialm = "GitHub: janithsadanuwan" //ur github or insta name
+global.ytname = "YT: Sameera C Athapaththu" //ur yt chanel name
+global.socialm = "GitHub: sameera_c_athapaththu" //ur github or insta name
 global.location = "Srilanka, colombo" //ur location
 
